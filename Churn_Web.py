e7f891cf567b8189b4732a69f6f280899ac7fd62
Haitham Abdelfattah Mahmoud Fathy <haithamfathy397@gmail.com>
2025-09-06 21:06:30 +0300

    Add files via upload

--- Churn_Web.py
+++ Churn_Web.py
@@ -3,38 +3,63 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 import joblib 
+import base64
+
+
+# Function to add offline background
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+    
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/png;base64,{encoded}");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+# Call background function
+set_background("backgound.png")  
+
 
 # Title
-
 st.title(" Customer Churn Prediction ")
 
 st.write("Enter customer details to predict if they will churn")
 
+# Load dataset and model
 df = pd.read_excel("churn_dataset.xlsx")
-
 model = joblib.load("Churn_Model.pkl")
 
-target={1:"churn",0:"not churn"}
+target = {1: "churn", 0: "not churn"}
 
-
-age = st.number_input("Age", min_value=18, max_value=100, value=30)
-gender = st.selectbox("Gender", ["Male", "Female"])
-tenure = st.slider("Tenure (Months)", min_value=0, max_value=100, value=12)
+# Inputs
+age = st.sidebar.number_input("Age", min_value=18, max_value=100, value=30)
+gender = st.sidebar.selectbox("Gender", ["Male", "Female"])
+tenure = st.sidebar.slider("Tenure (Months)", min_value=0, max_value=100, value=12)
 
 gender_encoded = 1 if gender == "Male" else 0
+input_data = np.array([[age, gender_encoded, tenure]])
 
-input_data = np.array([[age,gender_encoded,tenure]])
+# Predictions
+prediction = model.predict(input_data)[0]
+prediction_prob = model.predict_proba(input_data)[0]
 
-predicition = model.predict(input_data)[0]
+# Results
+st.subheader("Prediction Result")
+st.write(f"Customer is {target[prediction]}")
 
-predicition_prob = model.predict_proba(input_data)[0]
+st.subheader("Prediction Probability Result")
+st.write(f"Probability of churn: {prediction_prob[1]:.2f}")
+st.write(f"Probability of no churn: {prediction_prob[0]:.2f}")
 
-
-
-st.subheader("predection result")
-st.write(f"Customer is {target[predicition]}")
-
-
-st.subheader("predection probabilty result")
-st.write(f"Probability of churn: {predicition_prob[1]:.2f}")
-st.write(f"Probability of no churn: {predicition_prob[0]:.2f}")
+if prediction == 1:
+    st.error("⚠️ Customer is likely to churn!")
+else:
+    st.success("✅ Customer is not likely to churn.")
